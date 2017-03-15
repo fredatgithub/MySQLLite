401afc53edb3c465002d402fd62229daca29da57
wirlsawyer <wirlsawyer@gmail.com>
2017-03-15 14:11:29 +0800

    First repository

---
+++ MySQLLite/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace MySQLLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablesAsCSVFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SQLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(4, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(517, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(528, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 31);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(557, 270);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 338);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(566, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(129, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(566, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablesAsCSVFileToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // tablesAsCSVFileToolStripMenuItem
+            // 
+            this.tablesAsCSVFileToolStripMenuItem.Name = "tablesAsCSVFileToolStripMenuItem";
+            this.tablesAsCSVFileToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.tablesAsCSVFileToolStripMenuItem.Text = "Table(s) as CSV file...";
+            this.tablesAsCSVFileToolStripMenuItem.Click += new System.EventHandler(this.tablesAsCSVFileToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(188, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.exitToolStripMenuItem.Text = "Exit (&X)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SQLToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // SQLToolStripMenuItem
+            // 
+            this.SQLToolStripMenuItem.Name = "SQLToolStripMenuItem";
+            this.SQLToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.SQLToolStripMenuItem.Text = "SQL";
+            this.SQLToolStripMenuItem.Click += new System.EventHandler(this.SQLToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 290);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(517, 45);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(527, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 360);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MySQL Lite v1.00.01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablesAsCSVFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SQLToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MySQLLite/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQLLite
+{
+    public partial class Form1 : Form
+    {
+        static String DB_NAME = "tablename";
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Clear();
+            ArrayList tables = MySQL.GetTables(DB_NAME);
+            foreach (String name in tables)
+            {
+                comboBox1.Items.Add(name);
+            }
+
+            dataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.RowTemplate.Height = 50;
+
+            toolStripStatusLabel1.Text = "";
+
+            String SQL = "SELECT DISTINCT a.product_sn, a.*, b.* FROM StationTestReport AS a, DevFinalTestReport AS b WHERE a.device_pcb_sn LIKE b.device_pcb_sn";
+            textBox1.Text = SQL;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int w = this.Width - 25;
+            button1.Left = w - button1.Width + 5;
+            comboBox1.Width = w - button1.Width - 10;
+
+            dataGridView1.Width = w;
+            dataGridView1.Height = this.Height - dataGridView1.Top - statusStrip1.Height - 45;
+
+            if (textBox1.Visible)
+            {
+                button2.Left = button1.Left;
+                textBox1.Width = w - button2.Width-5;
+                textBox1.Top = dataGridView1.Top + dataGridView1.Height - 45;
+                button2.Top = textBox1.Top;
+                dataGridView1.Height -= (45 + 5);
+            }
+      
+        }
+
+        private void freshData(String tableName, String sql)
+        {
+            toolStripStatusLabel1.Text = sql;
+
+            MySQLData data = MySQL.GetData(DB_NAME, tableName, sql);
+
+            if (data.E.Message.Length > 0)
+            {
+                toolStripStatusLabel1.Text = data.E.Message;
+                return;
+            }
+            dataGridView1.ColumnCount = data.Columns.Count;
+            for (int i = 0; i < data.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].Name = (String)data.Columns[i];
+            }
+
+            dataGridView1.Rows.Clear();
+            foreach (Dictionary<String, String> dict in data.Datas)
+            {
+                ArrayList itemList = new ArrayList();
+
+                foreach (String title in data.Columns)
+                {
+                    itemList.Add(dict[title]);
+                }
+                dataGridView1.Rows.Add(itemList.ToArray());
+            }
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+            }
+
+          
+            /*
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.Width = 200;
+            buttonColumn.HeaderText = "按o鈕s";
+            buttonColumn.Name = "Status Request";
+            buttonColumn.Text = "Request Status";
+            buttonColumn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(buttonColumn);
+            // Add a CellClick handler to handle clicks in the button column.
+            //dataGridView1.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+            */
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //{ "debug":1, "mode":"1", "sql":"SELECT * FROM devagingtestreport", "table":"devagingtestreport"}
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("請先選擇一個資料表", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            String tableName = comboBox1.SelectedItem.ToString();
+            String sql = "SELECT * FROM " + tableName;
+            
+            freshData(tableName, sql);
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            comboBox1_SelectedIndexChanged(comboBox1, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void tablesAsCSVFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("請先選擇一個資料表", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "CSV File|*.csv";
+            saveFileDialog1.Title = "Save an CSV File";
+            saveFileDialog1.FileName = comboBox1.SelectedItem.ToString() +" "+DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+            saveFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName == "")
+            {               
+                return;
+            }
+
+
+            string delimiter = ",";
+            string fullFilename = saveFileDialog1.FileName;
+            StreamWriter csvStreamWriter = new StreamWriter(fullFilename, false, System.Text.Encoding.UTF8);
+            //output header data
+            string strHeader = "";
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                strHeader += dataGridView1.Columns[i].HeaderText + delimiter;
+            }
+            csvStreamWriter.WriteLine(strHeader);
+
+            //output rows data
+            for (int j = 0; j < dataGridView1.Rows.Count; j++)
+            {
+                string strRowValue = "";
+
+                for (int k = 0; k < dataGridView1.Columns.Count; k++)
+                {
+                    strRowValue += dataGridView1.Rows[j].Cells[k].Value + delimiter;
+
+                }
+                csvStreamWriter.WriteLine(strRowValue);
+            }
+
+            csvStreamWriter.Close();
+
+            MessageBox.Show("儲存完畢", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SQLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLToolStripMenuItem.Checked = !SQLToolStripMenuItem.Checked;
+            textBox1.Visible = SQLToolStripMenuItem.Checked;
+            button2.Visible = textBox1.Visible;
+            Form1_SizeChanged(this, null);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            freshData("StationTestReport", textBox1.Text);
+        }
+    }
+}
